c5e8073d1faefdefd24ed3da825393674953a423
martinc1511 <37803701+martinc1511@users.noreply.github.com>
2018-03-26 15:58:51 +0100

    Add files via upload

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,9 +4,8 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-
         public StandardGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Standard;
