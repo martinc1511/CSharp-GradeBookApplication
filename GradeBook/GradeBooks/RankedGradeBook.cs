5b1511f07568c80286a304fd45857f820bc4b6eb
martinc1511 <37803701+martinc1511@users.noreply.github.com>
2018-03-28 14:20:32 +0100

    Add files via upload

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,43 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Linq;
-using GradeBook.Enums;
-
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Ranked;
+            Type = Enums.GradeBookType.Ranked;
         }
 
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Ranked-grading requires a minimum of students to work");
+                throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
             }
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            //return base.GetLetterGrade(averageGrade);
-
-            if (grades[threshold - 1] <=averageGrade)
+            if (averageGrade >= grades[threshold - 1])
                 return 'A';
-            else if (grades[(threshold * 2) - 1] <=averageGrade)
+            if (averageGrade >= grades[(threshold * 2) - 1])
                 return 'B';
-            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            if (averageGrade >= grades[(threshold * 3) - 1])
                 return 'C';
-            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            if (averageGrade >= grades[(threshold * 4) - 1])
                 return 'D';
-
             return 'F';
-
-
         }
-
     }
 }
