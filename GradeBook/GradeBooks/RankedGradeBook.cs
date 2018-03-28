d900d511c9d0160f4bee15862acf58beddc77bab
martinc1511 <37803701+martinc1511@users.noreply.github.com>
2018-03-28 14:26:34 +0100

    Add files via upload

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,5 +29,19 @@ namespace GradeBook.GradeBooks
                 return 'D';
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException
+                    ("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
     }
 }
