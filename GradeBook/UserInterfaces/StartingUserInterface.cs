54985245edcc5679b37f79bb5b7728fd8a29525d
martinc1511 <37803701+martinc1511@users.noreply.github.com>
2018-03-28 15:09:37 +0100

    Add files via upload

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -39,7 +39,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2];
+            var type = parts[2].ToLower();
 
             BaseGradeBook gradeBook;
             if (type == "standard")
